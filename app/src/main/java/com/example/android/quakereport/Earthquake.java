0fd60fe4747b4556fbea2531d6ae2ba439ad408c
pokehei2 <pokehei2@yahoo.com.hk>
2017-06-29 00:44:19 +0800

    Android basics Networking - Lesson 1, earthquake - 0.1

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,34 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+
+    private String mMagnitude;
+    private String mLocation;
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
+
+    /**
+     * Constructs a new {@link Earthquake} object.
+     *
+     * @param magnitude is the magnitude (size) of the earthquake
+     * @param location is the city location of the earthquake
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *  earthquake happened
+     */
+    public Earthquake(String magnitude, String location, long timeInMilliseconds) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+    }
+
+    public String getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() { return mLocation; }
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() { return mTimeInMilliseconds; }
+
+}
